ed0019b18a6276226de03d4739c0216f2937eb4d
jessica1401 <jessica.gulati@pintar.co>
2022-04-20 01:42:00 +0530

    data transformation, success and error callbacks, useQuery on click

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,20 +2,33 @@ import { useQuery } from 'react-query';
 import axios from 'axios';
 
 const fetchAlltodos = () => {
-  return axios.get('https://jsonplaceholder.typicode.com/todos/')
+  return axios.get('https://jsonplaceholder.typicode.com/todos')
 }
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery('super-heros', fetchAlltodos);
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery('super-heros', fetchAlltodos, { 
+    // enabled: false
+    select: (data) => {
+      const allTodos = data.data.map(todo => todo.title);
+      return allTodos;
+    }
+  });
+  console.log(isFetching);
   if (isLoading) {
     return <h2>Is Loading ...</h2>;
+  }
+
+  if(isError) {
+    return <h2>{error.message}</h2>
   }
   return (
     <>
       <h2>Todo List</h2>
-      {data?.data.map((hero) => {
+      <button onClick={refetch}>Clickl To Display</button>
+      {/* {data?.data.map((hero) => {
         return <div key={hero.id}>{hero.title}</div>;
-      })}
+      })} */}
+      {data.map(todo => <div key={todo}>{todo}</div>)}
     </>
   );
 };
